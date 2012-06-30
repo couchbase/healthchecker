0a35e35f438cdb1f81762bac701e0cc4d4b341fb
Benjamin Young <byoung@bigbluehat.com>
2012-06-30 10:21:13 -0400

    moved reports nav to top
    Reports nav is now a scroll-able history view of
    all reports available in the reports folder. The
    intent is to eventually color-ize the report links
    to give a quick view of red/green statuses of past
    reports
    
    Change-Id: Ie48fac320cf4cc525b96d931efd79ec99baac229

--- reports/app.js
+++ reports/app.js
@@ -18,11 +18,73 @@
  **/
 
 Zepto(function($) {
+  /** utility functions **/
+  function pxToNumber(css_px_value) {
+    return Number(css_px_value.replace('px', ''));
+  }
+
+  /** DOM stuff **/
   $.getJSON('all.json',
   function(data) {
-    $reports = $('<ul>').appendTo('#sidebar');
-    $.each(data, function(key, val) {
-      $reports.append('<li><a href="' + val + '.html">' + val + '</a></li>');
+    dates = _.groupBy(data,
+        function(name) {
+          return name.split('_')[0]
+        });
+
+    thead = '<thead><tr>';
+    $.each(dates, function(key, val) {
+      thead += '<th colspan="' + val.length + '">' + key + '</th>';
+    });
+    thead += '</tr></thead>';
+
+    tbody = '<tbody><tr>';
+    $.each(dates, function(key, val) {
+      $.each(val, function(i, time) {
+        tbody += '<td><a href="' +  time + '.html">'
+              + time.split('_')[1].substr(0, 5).replace('-', ':')
+              + '</a></td>';
+      });
+    });
+    tbody += '</tr></tbody>';
+
+    $table = $('<table>' + thead + tbody + '</table>')
+      .appendTo('#reports');
+
+    var intervalID,
+        right_start = pxToNumber($table.css('right')),
+        left_start = ($(window).width()
+                      - ($(window).width()-$('#next').offset().left)
+                      - $table.width());
+    function startMoving(direction) {
+      intervalID = setInterval(function() { move(direction); }, 50);
+    }
+
+    function move(direction) {
+      var right = pxToNumber($table.css('right')),
+          left = pxToNumber($table.css('left'));
+      if (direction === 'right' && right >= right_start) {
+        stopMoving();
+      } else if (direction === 'left' && right <= left_start) {
+        stopMoving();
+      } else {
+        $table.animate({right: right + (direction === 'left' ? -10 : 10)});
+      }
+    }
+
+    function stopMoving() {
+      clearInterval(intervalID);
+    }
+
+    $('#prev').on('mouseenter', function() {
+      startMoving('left');
+    }).on('mouseleave', function() {
+      stopMoving();
+    });
+
+    $('#next').on('mouseenter', function() {
+      startMoving('right');
+    }).on('mouseleave', function() {
+      stopMoving();
     });
   });
-})
+});
