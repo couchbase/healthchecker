a11c5f80a7b02a2a44f3d8587fd123f2609d68b1
Bin Cui <bin.cui@gmail.com>
2013-07-16 12:09:58 -0700

    MB-8366: Fix nested table bug
    When using jquery for table selector, we should only focus on top
    level table and ignore nested ones.
    
    Change-Id: I0190770e1d05a5a61bc1520383813edd253e8831

--- reports/expandtable.js
+++ reports/expandtable.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-    $("#report tr:odd").addClass("odd");
-    $("#report tr:not(.odd)").hide();
-    $("#report tr:first-child").show();
-    $("#embeded tr").show()
-    $("#embeded tr.odd").removeClass("odd")
-    $("#report tr.odd").click(function(){
-        $(this).next("tr").toggle();
-        $(this).find(".arrow").toggleClass("up");
-    });
+  $("table#report > tbody > tr:even").addClass("even");
+  $("table#report > tbody > tr:odd").hide()
+  $("table#report > tbody > tr.even").click(function(){
+    $(this).next("tr").toggle();
+    $(this).find(".arrow").toggleClass("up");
+  });
 });
